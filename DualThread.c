6e31f50b2395dcb842d6be43602790478390c9ee
abdullahsultan <abdullahsultan448@gmail.com>
2020-03-05 02:26:09 +0500

    bugDualCore

--- DualThread.c
+++ DualThread.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define row 50
-#define col 50
+#define row 4
+#define col 4
 
 struct matrices{
   int matrixA[row][col];
   int matrixB[row][col];
   int matrixC[row][col];
-}
+};
 
 
 void *threadA(void *m)
@@ -19,30 +19,32 @@ void *threadA(void *m)
   mat = (struct matrices*) m;
 
     int i, j, k;
+
         for (i = 0; i < row/2; i++)
         {
             for (j = 0; j < row/2; j++)
             {
-                mat.matrixC[i][j] = 0;
+                mat->matrixC[i][j] = 0;
                 for (k = 0; k < row/2; k++)
-                    mat.matrixC[i][j] += mat.matrixA[i][k]*mat.matrixB[k][j];
+                    mat->matrixC[i][j] += mat->matrixA[i][k]*mat->matrixB[k][j];
             }
         }
 }
 
-void *threadB(void *mat)
+void *threadB(void *m)
 {
   struct matrices *mat;
   mat = (struct matrices*) m;
 
     int i, j, k;
+
         for (i = row/2; i < row; i++)
         {
             for (j = row/2; j < row; j++)
             {
-                mat.matrixC[i][j] = 0;
+                mat->matrixC[i][j] = 0;
                 for (k = row/2; k < row; k++)
-                    mat.matrixC[i][j] += mat.matrixA[i][k]*mat.matrixB[k][j];
+                    mat->matrixC[i][j] += mat->matrixA[i][k]*mat->matrixB[k][j];
             }
         }
 }
@@ -55,23 +57,33 @@ struct matrices mat;
 
   for (int i = 0; i < row; i++) {
     for (int j = 0; j < col; j++) {
-        mat.matrixA[i][j] = (rand() % (10 - 1 + 1)) + 1;
-        mat.matrixB[i][j] = (rand() % (10 - 1 + 1)) + 1;
+        mat.matrixA[i][j] = (rand() % (5 - 1 + 1)) + 1;
+        mat.matrixB[i][j] = (rand() % (5 - 1 + 1)) + 1;
+    }
+  }
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      mat.matrixC[i][j]=0;
     }
   }
 
   pthread_t ida,idb;
 
-  pthread_create(&ida, NULL, threadA, (void *)mat);
-  pthread_create(&idb,NULL, threadB, (void*)mat);
+  pthread_create(&ida, NULL, threadA, (void *)&mat);
+ pthread_create(&idb, NULL, threadB, (void*)&mat);
 
 
-/*for (i = 0; i < row; i++) {
+ sleep(2);
+
+int i, j;
+for (i = 0; i < row; i++) {
   printf("\n");
   for (j = 0; j < col; j++) {
     printf("%d    ", mat.matrixA[i][j]);
   }
 }
+
+printf("\n\n\n");
 
 for (i = 0; i < row; i++) {
   printf("\n");
@@ -80,12 +92,19 @@ for (i = 0; i < row; i++) {
   }
 }
 
-for (i = 0; i < row; i++) {
+printf("\n\n\n");
+
+for ( i = 0; i < row; i++) {
   printf("\n");
-  for (j = 0; j < col; j++) {
+  for ( j = 0; j < col; j++) {
     printf("%d    ", mat.matrixC[i][j]);
   }
 }
-*/
+
+
+while (1) {
+  /* code */
+}
+
   return 0;
 }
